275481c2283c883dc51f174e6fadc63d6f83cf2a
davecar21 <davecargrave@gmail.com>
2018-06-18 23:54:55 +0800

    nodemailer Test

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
+
+const nodemailer = require('nodemailer');
+
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
 
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
+
+(function() {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'davecargrave@gmail.com',
+            pass: 'Dav3car21'
+        }
+    });
+
+    const mailOptions = {
+        from: 'davecargrave@gmail.com', // sender address
+        to: 'davecargrave@gmail.com', // list of receivers
+        subject: 'Subject of your email', // Subject line
+        html: '<p>Your TEST here</p>' // plain text body
+    };
+
+    transporter.sendMail(mailOptions, function(err, info) {
+        if (err)
+            console.log(err)
+        else
+            console.log(info);
+    });
+})();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'forms', component: FormComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
