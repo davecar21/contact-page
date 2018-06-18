9ea67d0dde5ee956dc197ea543e30700c6ee42a6
davecar21 <davecargrave@gmail.com>
2018-06-19 00:35:27 +0800

    gmail

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.listen(process.env.PORT || 8080);
 
 var transporter = nodemailer.createTransport({
     service: 'yahoo',
-    secure: false,
+    secure: true,
     auth: {
-        user: 'davecargrave21@yahoo.com',
+        user: 'davecargrave21@gmail.com',
         pass: 'Dav3car21'
     },
     tls: {
@@ -33,7 +33,7 @@ var transporter = nodemailer.createTransport({
 });
 
 const mailOptions = {
-    from: '"Davecar" <davecargrave21@yahoo.com>', // sender address
+    from: '"Davecar" <davecargrave21@gmail.com>', // sender address
     to: 'davecargrave21@gmail.com', // list of receivers
     subject: 'Subject of your email', // Subject line
     html: '<p>Your TEST here</p>' // plain text body
